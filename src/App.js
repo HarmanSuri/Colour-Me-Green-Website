a243a839c88304a6babeb58f81d0a10445f5bcf8
Aryan Soni <mail.aryansoni@gmail.com>
2019-12-11 11:54:42 -0500

    frontInfo component set up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-
+import frontInfo from "./components/frontInfo";
 function App() {
   return (
     <Router>
       <div className='App'>
         <div className='container'>
-          <Route path='/' component={Navbar} />
+          <Navbar />
+          <Route path='/' component={frontInfo} />
         </div>
       </div>
     </Router>
---
+++ src/components/frontInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class frontInfo extends Component {
+  render() {
+    return (
+      <div>
+        <p>HI</p>
+      </div>
+    );
+  }
+}
+
+export default frontInfo;
