6f409376b1125ba6cf0c92c35fc90ae00bde9685
MegaHSS <harmansuri@hotmail.com>
2019-12-11 15:14:54 -0500

    Added Charity

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Charity from "./components/Charity";
 import Navbar from "./components/Navbar";
 import frontInfo from "./components/frontInfo";
+
 function App() {
   return (
     <Router>
@@ -13,6 +15,7 @@ function App() {
         <div className='container'>
           <Navbar />
           <Route path='/' component={frontInfo} />
+          <Charity />
         </div>
       </div>
     </Router>
