2c7158b21d887a105f533aaffd873692d610adab
Aryan Soni <mail.aryansoni@gmail.com>
2019-12-11 11:48:52 -0500

    integrate component stucture

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <p>HI</p>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
